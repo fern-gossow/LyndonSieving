3ea53e90fc278fddca09c2cddf39aef85c0a8146
fern-gossow <139264117+fern-gossow@users.noreply.github.com>
2024-09-23 11:04:51 +1000

    Bug-testing and refactoring for simplicity

---
+++ package/LyndonSieving/polynomials.m
@@ -0,0 +1,36 @@
+// Construct and test Lyndon families of polynomials
+
+intrinsic QNumber(n :: RngIntElt) -> RngUPolElt
+{Return the q-analogue [n]_q=1+q+...+q^(n-1)}
+    R<q> := PolynomialRing(Integers());
+    if n le 0 then
+        return Zero(R);
+    end if;
+    return &+[q^i : i in [0..n-1]];
+end intrinsic;
+
+intrinsic QMultinomial(a :: [RngIntElt]) -> RngUPolElt
+{Return the q-multinomial [a_1,a_2,...]_q!=[a_1+a_2+...]_q/([a_1]_q[a_2]_q...)}
+    R<q> := PolynomialRing(Integers());
+    // If composition contains negative values return 0
+    if &or[x lt 0 : x in a] then
+        return Zero(R);
+    elif #a eq 0 or &+a eq 0 then
+        return One(R);
+    end if;
+    qnums := [&+[q^i : i in [0..m-1]] : m in [1..&+a]];
+    qmult := &*qnums[1..&+a]/&*[&*qnums[1..x] : x in a | x gt 0];
+    // Ensure the divison was completed
+    assert Denominator(qmult) eq One(R);
+    return Numerator(qmult);
+end intrinsic;
+
+intrinsic QFactorial(n :: RngIntElt) -> RngUPolElt
+{Return the q-factorial [n]_q!=[1]_q...[n]_q}
+    return QMultinomial([n]);
+end intrinsic;
+
+intrinsic QBinomial(n :: RngIntElt, k :: RngIntElt) -> RngUPolElt
+{Return the q-binomial [(n,k)]_q!=([n]_q...[n-k+1]_q)/([k]_q...[1]_q)}
+    return QMultinomial([k,n-k]);
+end intrinsic;
---
+++ package/LyndonSieving/polynomials.sig
@@ -0,0 +1,5 @@
+166,0
+S,QNumber,Return the q-analogue [n]_q=1+q+...+q^(n-1),0,1,0,0,0,0,0,0,0,148,,312,-38,-38,-38,-38
+S,QMultinomial,"Return the q-multinomial [a_1,a_2,...]_q!=[a_1+a_2+...]_q/([a_1]_q[a_2]_q...)",1,0,1,82,0,148,1,0,0,0,0,0,0,0,82,,312,-38,-38,-38,-38
+S,QFactorial,Return the q-factorial [n]_q!=[1]_q...[n]_q,0,1,0,0,0,0,0,0,0,148,,312,-38,-38,-38,-38
+S,QBinomial,"Return the q-binomial [(n,k)]_q!=([n]_q...[n-k+1]_q)/([k]_q...[1]_q)",0,2,0,0,0,0,0,0,0,148,,0,0,148,,312,-38,-38,-38,-38
