b87dede03c399585b77b08f61373feb3db53f36a
fern-gossow <139264117+fern-gossow@users.noreply.github.com>
2024-09-23 15:28:14 +1000

    Gauss congruence testing (single variable)

--- package/LyndonSieving/structures.m
+++ package/LyndonSieving/structures.m
@@ -3,11 +3,20 @@
 intrinsic RamanujanSum(d::RngIntElt, j::RngIntElt) -> RngIntElt
 {Return the Ramanujan sum c_j(d), sum of the jth powers of dth primitve roots of unity}
     requirege d, 1;
-    j := j mod d;
-    if j eq 0 then
-        j := d;
-    end if;
     x := ExactQuotient(d,GCD(j,d));
     return MoebiusMu(x)*ExactQuotient(EulerPhi(d),EulerPhi(x));
 end intrinsic;
 
+intrinsic TestGaussCongruence(X::UserProgram, n::RngIntElt) -> BoolElt
+{Tests where the values of X satisfy the Gauss congruence for n, i.e. Sum(mu(n/d)X(d),d|n)=0 (mod n)}
+    require n ge 1: "n must be positive";
+    require &and[Type(X(d)) eq RngIntElt : d in Divisors(n)]: "X must return integer values";
+    return &+[MoebiusMu(ExactQuotient(n,d))*X(d) : d in Divisors(n)] mod n eq 0;
+end intrinsic;
+
+intrinsic TestGaussCongruenceFull(X::UserProgram, n::RngIntElt) -> BoolElt
+{Tests where the values of X satisfy the Gauss congruence for n and all values of the Ramanujan sum}
+    require n ge 1: "n must be positive";
+    require &and[Type(X(d)) eq RngIntElt : d in Divisors(n)]: "X must return integer values";
+    return &and[&+[RamanujanSum(ExactQuotient(n,d),j)*X(d) : d in Divisors(n)] mod n eq 0 : j in Divisors(n)];
+end intrinsic;
--- package/LyndonSieving/structures.sig
+++ package/LyndonSieving/structures.sig
@@ -1,2 +1,4 @@
 166,0
 S,RamanujanSum,"Return the Ramanujan sum c_j(d), sum of the jth powers of dth primitve roots of unity",0,2,0,0,0,0,0,0,0,148,,0,0,148,,148,-38,-38,-38,-38
+S,TestGaussCongruence,"Tests where the values of X satisfy the Gauss congruence for n, i.e. Sum(mu(n/d)X(d),d|n)=0 (mod n)",0,2,0,0,0,0,0,0,0,148,,0,0,41,,36,-38,-38,-38,-38
+S,TestGaussCongruenceFull,Tests where the values of X satisfy the Gauss congruence for n and all values of the Ramanujan sum,0,2,0,0,0,0,0,0,0,148,,0,0,41,,36,-38,-38,-38,-38
