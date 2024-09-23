68ddc136dc4c5b9870c28dba8ae2385acf9259f0
fern-gossow <139264117+fern-gossow@users.noreply.github.com>
2024-09-23 13:13:54 +1000

    Testing Lyndon families of polynomials

--- package/LyndonSieving/polynomials.m
+++ package/LyndonSieving/polynomials.m
@@ -9,7 +9,7 @@ intrinsic QNumber(n :: RngIntElt) -> RngUPolElt
     return &+[q^i : i in [0..n-1]];
 end intrinsic;
 
-intrinsic QFactorial := function(n :: RngIntElt) -> RngUPolElt
+intrinsic QFactorial(n :: RngIntElt) -> RngUPolElt
 {Return the q-factorial [n]_q!=[1]_q...[n]_q}
     R<q> := PolynomialRing(Integers());
     if n le 0 then
@@ -29,10 +29,7 @@ intrinsic QMultinomial(a :: [RngIntElt]) -> RngUPolElt
         return One(R);
     end if;
     qnums := [&+[q^i : i in [0..m-1]] : m in [1..&+a]];
-    qmult := &*qnums[1..&+a]/&*[&*qnums[1..x] : x in a | x gt 0];
-    // Ensure the divison was completed
-    assert Denominator(qmult) eq One(R);
-    return Numerator(qmult);
+    return ExactQuotient(&*qnums[1..&+a], &*[&*qnums[1..x] : x in a | x gt 0]);
 end intrinsic;
 
 intrinsic QBinomial(n :: RngIntElt, k :: RngIntElt) -> RngUPolElt
@@ -40,3 +37,37 @@ intrinsic QBinomial(n :: RngIntElt, k :: RngIntElt) -> RngUPolElt
     return QMultinomial([k,n-k]);
 end intrinsic;
 
+intrinsic TestLyndonFamily(f :: UserProgram, n :: RngIntElt) -> BoolElt
+{Given a polynomial family f_n (as f(n)=f_n), test the Lyndon family condition by substituting roots of unity}
+    requirege n, 1;
+    for d in Divisors(n) do
+        require Type(f(d)) eq RngUPolElt: "Outputs of f must be integer polynomials";
+    end for;
+    A<z> := CyclotomicField(n);
+    return &and[Evaluate(f(n), z^d) eq Evaluate(f(d), One(A)) : d in Divisors(n)];
+end intrinsic;
+
+intrinsic TestLyndonFamily(f :: UserProgram, rk :: RngIntElt, s :: [RngIntElt]) -> BoolElt
+{Given a polynomial family indexed by s and given rank, test the Lyndon family condition by substituting roots of unity}
+    require rk ge 1: "Rank must be positive";
+    s_divisors := &meet[Set(Divisors(a)) : a in s];
+    require s_divisors subset Divisors(rk) : "Divisors of s must be divisors of rk";
+    for d in s_divisors do
+        require Type(f([ExactQuotient(a,d) : a in s])) eq RngUPolElt: "Outputs of f must be integer polynomials";
+    end for;
+    A<z> := CyclotomicField(rk);
+    // Evaluate for all divisors of rk, check Lyndon condition
+    for d in Divisors(rk) do
+        if d in s_divisors then
+            if not Evaluate(f(s), z^ExactQuotient(rk,d)) eq Evaluate(f([ExactQuotient(a, d) : a in s]), One(A)) then
+                return false;
+            end if;
+        else
+            if not Evaluate(f(s), z^ExactQuotient(rk,d)) eq Zero(A) then
+                return false;
+            end if;
+        end if;
+    end for;
+    // If all checks are passed, return true
+    return true;
+end intrinsic;
--- package/LyndonSieving/polynomials.sig
+++ package/LyndonSieving/polynomials.sig
@@ -1,5 +1,7 @@
 166,0
 S,QNumber,Return the q-analogue [n]_q=1+q+...+q^(n-1),0,1,0,0,0,0,0,0,0,148,,312,-38,-38,-38,-38
-S,QMultinomial,"Return the q-multinomial [a_1,a_2,...]_q!=[a_1+a_2+...]_q/([a_1]_q[a_2]_q...)",1,0,1,82,0,148,1,0,0,0,0,0,0,0,82,,312,-38,-38,-38,-38
 S,QFactorial,Return the q-factorial [n]_q!=[1]_q...[n]_q,0,1,0,0,0,0,0,0,0,148,,312,-38,-38,-38,-38
+S,QMultinomial,"Return the q-multinomial [a_1,a_2,...]_q!=[a_1+a_2+...]_q/([a_1]_q[a_2]_q...)",1,0,1,82,0,148,1,0,0,0,0,0,0,0,82,,312,-38,-38,-38,-38
 S,QBinomial,"Return the q-binomial [(n,k)]_q!=([n]_q...[n-k+1]_q)/([k]_q...[1]_q)",0,2,0,0,0,0,0,0,0,148,,0,0,148,,312,-38,-38,-38,-38
+S,TestLyndonFamily,"Given a polynomial family f_n (as f(n)=f_n), test the Lyndon family condition by substituting roots of unity",0,2,0,0,0,0,0,0,0,148,,0,0,41,,36,-38,-38,-38,-38
+S,TestLyndonFamily,"Given a polynomial family indexed by s and given rank, test the Lyndon family condition by substituting roots of unity",1,2,1,82,0,148,3,0,0,0,0,0,0,0,82,,0,0,148,,0,0,41,,36,-38,-38,-38,-38
